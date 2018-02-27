4481d1705bb4b40540313ed9fad941311b19ee72
Vincent La <vincent.la@nokia.com>
2018-02-27 14:29:09 -0800

    adding in dynamic config files

--- src/config/index.js
+++ src/config/index.js
@@ -1,9 +1,28 @@
-export default {
+import * as _ from 'lodash';
+
+process.env.NODE_ENV = process.env.NODE_ENV || 'development';
+const env = process.env.NODE_ENV;
+
+const baseConfig = {
   db: {
     url: 'mongodb://localhost/dope'
   },
   expireTime: "30d",
   secrets: {
     JWT_SECRET: "test123"
-  }
+  },
+  port: '3000'
 }
+
+let devConfig;
+
+switch(env) {
+  case 'prod':
+  case 'production':
+    devConfig = require('./production.js');
+    break;
+  default:
+    devConfig = require("./development.js");
+}
+
+export default _.merge(baseConfig, devConfig);
